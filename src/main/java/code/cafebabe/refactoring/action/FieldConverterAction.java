4df42a23d379c78a708daa8cea321b45e1bd37b2
Sebastian <sebsen36@gmail.com>
2018-01-23 08:11:38 +0100

    Added new class FieldConverterAction

---
+++ src/main/java/code/cafebabe/refactoring/action/FieldConverterAction.java
@@ -0,0 +1,28 @@
+package code.cafebabe.refactoring.action;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.FieldDeclaration;
+
+public final class FieldConverterAction extends Action {
+
+	@Override
+	public void consume(List<Node> pNodesToProcess,
+			Optional<FieldDeclaration> matchingFieldDeclarationsForReplacementType) {
+		System.out.println(pNodesToProcess);
+	}
+
+	@Override
+	public void consumeFieldDeclarations(Set<FieldDeclaration> pFieldDeclarations) {
+		System.out.println(pFieldDeclarations);
+	}
+
+	@Override
+	public void consumeImports(CompilationUnit pCompilationUnit, Class<?> pTargetType) {
+	}
+
+}
