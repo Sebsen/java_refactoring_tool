cd1c49ab93d7ff88717ab4da38ab53f17a625a88
Sebastian <sebsen36@gmail.com>
2018-01-24 08:47:53 +0100

    Extended test setup -> When field with desired name already exists

---
+++ src/test/resources/targets/introduceFieldExistingFieldWithSameName/MethodCallClassTarget.java
@@ -0,0 +1,18 @@
+import java.io.FileNotFoundException;
+
+import introduceField.custom.Logger;
+import introduceField.custom.MessageLogger;
+
+public class MethodCallClass {
+	
+	private static final Logger myLogger = MessageLogger.instance();
+
+	public void print() {
+		try {
+			throw new FileNotFoundException();
+		} catch (FileNotFoundException e) {
+			myLogger.logAsInternalException(e);
+		}
+	}
+	
+}
